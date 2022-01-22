d7cc2e9b5f6a9843bfa0237f78c7bb40d8a5bbbf
awsome-cats <cattwin82@gmail.com>
2022-01-22 22:34:50 +0900

    node-fetch version from 2.6.0 to 3

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,11 @@
+<!-- eslint-disable -->
 <template>
   <div>
   <!---bootstrapの使用:tabs------>
   <b-nav tabs>
   <!-- question point--->
   <!-- nav barの使用だったため...disabledにすることでボタンを押せなくする--->
-  <b-nav-item disabled><b>Facny Quiz App</b></b-nav-item>          
+  <b-nav-item disabled><b>Facny Quiz App</b></b-nav-item>
   <b-nav-item disabled>Counter:{{ numCorrect }}/{{ numTotal }}</b-nav-item>
   </b-nav>
     </div>
